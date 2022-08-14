c08226ac6afa70ebcd386897bd8e69dfde8f1bfb
harukzz <108406720+harukzz@users.noreply.github.com>
2022-08-14 21:03:22 +1000

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './Containers/App';
-import registerServiceWorker from './registerServiceWorker';
-import 'tachyons';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
-//registerServiceWorker();
