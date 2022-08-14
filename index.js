0c01437a3dece2533526984b718e79b1a399e7f1
harukzz <108406720+harukzz@users.noreply.github.com>
2022-08-14 20:55:01 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './Containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
+//registerServiceWorker();
